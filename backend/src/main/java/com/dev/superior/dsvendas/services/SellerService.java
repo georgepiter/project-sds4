778764d6f8c8b682589b6be6ed9686b1b77f8e37
georgepiter <george.piter@terra.com.br>
2021-09-11 19:29:51 -0300

    Create Dto, controllers and repository

---
+++ backend/src/main/java/com/dev/superior/dsvendas/services/SellerService.java
@@ -0,0 +1,25 @@
+package com.dev.superior.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.superior.dsvendas.dto.SellerDto;
+import com.dev.superior.dsvendas.entities.Seller;
+import com.dev.superior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDto> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(r -> new SellerDto(r)).collect(Collectors.toList());		
+	}
+
+
+}
